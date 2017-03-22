fb0776cc93917433afc842f56017199de78012ab
Andre Odendaal <aodendaal@gmail.com>
2017-03-22 19:46:03 +0200

    Add player input and update font and color

--- js/game.js
+++ js/game.js
@@ -1,34 +1,140 @@
 var Game = {
     display: null,
+    map: {},
+    engine: null,
+    player: null,
+    ananas: null,
  
     init: function() {
-        this.display = new ROT.Display();
+        let options = {
+            fontSize: 20,
+            fontFamily: "metrickal, monospace"
+        };
+
+        this.display = new ROT.Display(options);
         document.body.appendChild(this.display.getContainer());
 
+        let context = document.createElement("canvas").getContext("2d");
+        context.font = "10px metrickal, monospace";
+
         this._generateMap();
-    }
-}
 
-Game.map = {};
-Game._generateMap = function() {
-    var digger = new ROT.Map.Digger();
- 
-    var digCallback = function(x, y, value) {
-        if (value) { return; } /* do not store walls */
- 
-        var key = x+","+y;
-        this.map[key] = ".";
-    }
-    digger.create(digCallback.bind(this));
+        var scheduler = new ROT.Scheduler.Simple();
+        scheduler.add(this.player, true);
 
-    this._drawWholeMap();
-}
+        this.engine = new ROT.Engine(scheduler);
+        this.engine.start();
+    },
 
-Game._drawWholeMap = function() {
-    for (var key in this.map) {
+    _generateMap: function() {
+        var digger = new ROT.Map.Digger();
+        var freeCells = [];
+    
+        var digCallback = function(x, y, value) {
+            if (value) { return; } /* do not store walls */
+        
+            var key = x+","+y;
+            freeCells.push(key);
+            this.map[key] = ".";
+        }
+        digger.create(digCallback.bind(this));
+    
+        this._generateBoxes(freeCells);
+        this._drawWholeMap();
+        this._createPlayer(freeCells);
+    },
+
+    _drawWholeMap: function() {
+        for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+            this.display.draw(x, y, this.map[key]);
+        }
+    },
+
+    _generateBoxes: function(freeCells) {
+        for (var i=0; i < 10; i++) {
+            var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+            var key = freeCells.splice(index, 1)[0];
+            this.map[key] = "*";
+            if (!i) {
+                this.ananas = key;
+            }
+        }
+    },
+
+    _createPlayer: function(freeCells) {
+        var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+        var key = freeCells.splice(index, 1)[0];
         var parts = key.split(",");
         var x = parseInt(parts[0]);
         var y = parseInt(parts[1]);
-        this.display.draw(x, y, this.map[key]);
+        this.player = new Player(x, y);
+    }
+}
+
+var Player = function(x, y) {
+    this._x = x;
+    this._y = y;
+    this._draw();
+}
+ 
+Player.prototype._draw = function() {
+    Game.display.draw(this._x, this._y, "@", "#ff0");
+}
+
+Player.prototype.act = function() {
+    Game.engine.lock();
+    /* wait for user input; do stuff when user hits a key */
+    window.addEventListener("keydown", this);
+}
+ 
+Player.prototype.handleEvent = function(e) {
+    /* process user input */
+    var keyMap = {};
+    keyMap[38] = 0;
+    keyMap[33] = 1;
+    keyMap[39] = 2;
+    keyMap[34] = 3;
+    keyMap[40] = 4;
+    keyMap[35] = 5;
+    keyMap[37] = 6;
+    keyMap[36] = 7;
+ 
+    var code = e.keyCode;
+
+    if (code == 13 || code == 32) {
+        this._checkBox();
+        return;
+    }
+ 
+    if (!(code in keyMap)) { return; }
+ 
+    var diff = ROT.DIRS[8][keyMap[code]];
+    var newX = this._x + diff[0];
+    var newY = this._y + diff[1];
+ 
+    var newKey = newX + "," + newY;
+    if (!(newKey in Game.map)) { return; } /* cannot move in this direction */
+
+    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock();    
+}
+
+Player.prototype._checkBox = function() {
+    var key = this._x + "," + this._y;
+    if (Game.map[key] != "*") {
+        alert("There is no box here!");
+    } else if (key == Game.ananas) {
+        alert("Hooray! You found a pineapple and won this game.");
+        Game.engine.lock();
+        window.removeEventListener("keydown", this);
+    } else {
+        alert("This box is empty :-(");
     }
 }
