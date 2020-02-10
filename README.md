8dfa5c40766ca88b17ab48ce6ba0f4d5ded44ab8
Andre Odendaal <aodendaal@gmail.com>
2020-02-10 21:03:20 +0200

    Add pre-built ROT file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Roguelite - An exercise in making a rogue-like game
 
 Uses [rot.js](https://github.com/ondras/rot.js)
+
+Recently updated in time for [#7DRL Challenge 2020](https://itch.io/jam/7drl-challenge-2020)
--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ var Game = {
     engine: null,
     player: null,
     ananas: null,
- 
-    init: function() {
+
+    init: function () {
         let options = {
             fontSize: 20,
             fontFamily: "metrickal, monospace"
@@ -26,25 +26,25 @@ var Game = {
         this.engine.start();
     },
 
-    _generateMap: function() {
+    _generateMap: function () {
         var digger = new ROT.Map.Digger();
         var freeCells = [];
-    
-        var digCallback = function(x, y, value) {
+
+        var digCallback = function (x, y, value) {
             if (value) { return; } /* do not store walls */
-        
-            var key = x+","+y;
+
+            var key = x + "," + y;
             freeCells.push(key);
             this.map[key] = ".";
         }
         digger.create(digCallback.bind(this));
-    
+
         this._generateBoxes(freeCells);
         this._drawWholeMap();
         this._createPlayer(freeCells);
     },
 
-    _drawWholeMap: function() {
+    _drawWholeMap: function () {
         for (var key in this.map) {
             var parts = key.split(",");
             var x = parseInt(parts[0]);
@@ -53,8 +53,8 @@ var Game = {
         }
     },
 
-    _generateBoxes: function(freeCells) {
-        for (var i=0; i < 10; i++) {
+    _generateBoxes: function (freeCells) {
+        for (var i = 0; i < 10; i++) {
             var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
             var key = freeCells.splice(index, 1)[0];
             this.map[key] = "*";
@@ -64,7 +64,7 @@ var Game = {
         }
     },
 
-    _createPlayer: function(freeCells) {
+    _createPlayer: function (freeCells) {
         var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
         var key = freeCells.splice(index, 1)[0];
         var parts = key.split(",");
@@ -74,59 +74,59 @@ var Game = {
     }
 }
 
-var Player = function(x, y) {
+var Player = function (x, y) {
     this._x = x;
     this._y = y;
     this._draw();
 }
- 
-Player.prototype._draw = function() {
+
+Player.prototype._draw = function () {
     Game.display.draw(this._x, this._y, "@", "#ff0");
 }
 
-Player.prototype.act = function() {
+Player.prototype.act = function () {
     Game.engine.lock();
     /* wait for user input; do stuff when user hits a key */
     window.addEventListener("keydown", this);
 }
- 
-Player.prototype.handleEvent = function(e) {
+
+Player.prototype.handleEvent = function (e) {
     /* process user input */
     var keyMap = {};
-    keyMap[38] = 0;
-    keyMap[33] = 1;
-    keyMap[39] = 2;
-    keyMap[34] = 3;
-    keyMap[40] = 4;
-    keyMap[35] = 5;
-    keyMap[37] = 6;
-    keyMap[36] = 7;
- 
+    keyMap[38] = 0; // up arrow
+    keyMap[33] = 1; // page up (up right)
+    keyMap[39] = 2; // right arrow
+    keyMap[34] = 3; // page down (down right)
+    keyMap[40] = 4; // down arrow
+    keyMap[35] = 5; // end (down left)
+    keyMap[37] = 6; // left arrow
+    keyMap[36] = 7; // home (up left)
+
     var code = e.keyCode;
 
-    if (code == 13 || code == 32) {
+    if (code == 13 || code == 32) { // 13 = Enter & 32 = Space
         this._checkBox();
         return;
     }
- 
+
     if (!(code in keyMap)) { return; }
- 
+
     var diff = ROT.DIRS[8][keyMap[code]];
     var newX = this._x + diff[0];
     var newY = this._y + diff[1];
- 
+
     var newKey = newX + "," + newY;
     if (!(newKey in Game.map)) { return; } /* cannot move in this direction */
 
-    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
+    Game.display.draw(this._x, this._y, Game.map[this._x + "," + this._y]);
     this._x = newX;
     this._y = newY;
     this._draw();
     window.removeEventListener("keydown", this);
-    Game.engine.unlock();    
+    Game.engine.unlock();
 }
 
-Player.prototype._checkBox = function() {
+Player.prototype._checkBox = function () {
     var key = this._x + "," + this._y;
     if (Game.map[key] != "*") {
         alert("There is no box here!");
